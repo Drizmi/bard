3de1f8ec5dad565971845ed9b64e3323b640a157
Drizmi <michael.drizga@gmail.com>
2020-02-20 12:16:20 +0100

    Initialization step 3
    Building the bot, adding token

--- src/main/java/me/michael/Bot.java
+++ src/main/java/me/michael/Bot.java
@@ -1,4 +1,11 @@
 package me.michael;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Bot {
+  public static void main(String[] args) throws Exception
+  {
+    JDA api = new JDABuilder("131WgstioQaUTAdAu-UJ9BVI-8mBAtch").build();
+  }
 }
