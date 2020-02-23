f6c422c3ffb2eb468402a4497a588c038f621130
Drizmi <michael.drizga@gmail.com>
2020-02-23 23:50:03 +0100

    Build 1.0.1
    FUNGUJE !!!

--- src/main/java/me/michael/Bot.java
+++ src/main/java/me/michael/Bot.java
@@ -1,15 +1,20 @@
 package me.michael;
 
-import net.dv8tion.jda.api.AccountType;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
 
 public class Bot {
-  public static JDA jda;
+
+  private Bot() throws LoginException {
+
+    new JDABuilder()
+            .setToken("NjQ3NzI5MzU2MDM0MDE1MjMy.XlMBYQ.4e662ZkRZOi1nyRE5ZTc1mG7100")
+    .addEventListeners(new Listener())
+            .build();
+  }
 
   public static void main(String[] args) throws LoginException {
-    jda = new JDABuilder(AccountType.BOT).setToken("131WgstioQaUTAdAu-UJ9BVI-8mBAtch").build();
+    new Bot();
   }
 }
