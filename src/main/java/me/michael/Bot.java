61d1f5e14394405257072e2789b9ca1bbc690ee9
Drizmi <michael.drizga@gmail.com>
2020-02-23 23:22:31 +0100

    Simpler bot
    still not working

--- src/main/java/me/michael/Bot.java
+++ src/main/java/me/michael/Bot.java
@@ -1,11 +1,15 @@
 package me.michael;
 
+import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import javax.security.auth.login.LoginException;
+
 public class Bot {
-  public static void main(String[] args) throws Exception
-  {
-    JDA api = new JDABuilder("131WgstioQaUTAdAu-UJ9BVI-8mBAtch").build();
+  public static JDA jda;
+
+  public static void main(String[] args) throws LoginException {
+    jda = new JDABuilder(AccountType.BOT).setToken("131WgstioQaUTAdAu-UJ9BVI-8mBAtch").build();
   }
 }
